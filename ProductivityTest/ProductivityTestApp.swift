0fc2b002fa4d2830c13ff600875535c1b3091918
Vallykrie <nathansudiara@gmail.com>
2026-03-09 21:00:58 +0700

    Initial Commit

---
+++ ProductivityTest/ProductivityTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityTestApp.swift
+//  ProductivityTest
+//
+//  Created by Nathan Sudiara on 09/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
